1d1b10d073764a0ebd4789e057dc274b013a84b1
Doubtful85 <100766236+Doubtful85@users.noreply.github.com>
2022-06-12 10:32:49 -0500

    Initialize release-google-cloud-vision-v0.39.0

---
+++ google-cloud-firestore/test/google/cloud/firestore/client/cols_test.rb
@@ -0,0 +1,51 @@
+# Copyright 2017 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "helper"
+
+describe Google::Cloud::Firestore::Client, :cols, :mock_firestore do
+  it "retrieves collections" do
+    firestore_mock.expect :list_collection_ids, ["users", "lists", "todos"].to_enum, ["projects/#{project}/databases/(default)/documents", options: default_options]
+
+    col_enum = firestore.cols
+    _(col_enum).must_be_kind_of Enumerator
+
+    col_ids = col_enum.map do |col|
+      _(col).must_be_kind_of Google::Cloud::Firestore::CollectionReference
+
+      _(col.parent).must_be_kind_of Google::Cloud::Firestore::Client
+
+      col.collection_id
+    end
+    _(col_ids).wont_be :empty?
+    _(col_ids).must_equal ["users", "lists", "todos"]
+  end
+
+  it "retrieves collections using collections alias" do
+    firestore_mock.expect :list_collection_ids, ["users", "lists", "todos"].to_enum, ["projects/#{project}/databases/(default)/documents", options: default_options]
+
+    col_enum = firestore.collections
+    _(col_enum).must_be_kind_of Enumerator
+
+    col_ids = col_enum.map do |col|
+      _(col).must_be_kind_of Google::Cloud::Firestore::CollectionReference
+
+      _(col.parent).must_be_kind_of Google::Cloud::Firestore::Client
+
+      col.collection_id
+    end
+    _(col_ids).wont_be :empty?
+    _(col_ids).must_equal ["users", "lists", "todos"]
+  end
+end
---
+++ google-cloud-speech-v1p1beta1/CHANGELOG.md
@@ -0,0 +1,50 @@
+# Release History
+
+### 0.3.1 / 2020-05-26
+
+#### Bug Fixes
+
+* Removed unused google/cloud/common_resources_pb file
+* The long-running operations client honors the quota_project config
+
+### 0.3.0 / 2020-05-21
+
+#### Features
+
+* The quota_project can be set via configuration
+
+### 0.2.1 / 2020-05-05
+
+#### Documentation
+
+* Clarify that timeouts are in seconds.
+
+### 0.2.0 / 2020-04-22
+
+#### Features
+
+* Support for speech adaptation configuration.
+
+#### Bug Fixes
+
+* Operations client honors its main client's custom endpoint.
+
+### 0.1.2 / 2020-04-13
+
+#### Documentation
+
+* Various documentation and other updates.
+  * Expanded the readme to include quickstart and logging information.
+  * Added documentation for package and service modules.
+  * Fixed and expanded documentation for the two method calling conventions.
+  * Fixed some circular require warnings.
+
+### 0.1.1 / 2020-04-01
+
+#### Documentation
+
+* Update documentation for core proto types.
+
+### 0.1.0 / 2020-03-25
+
+Initial release.
