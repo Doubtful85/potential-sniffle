d8c1b63e3808d76e4a435d14c33d05bf665a4bc0
Doubtful85 <100766236+Doubtful85@users.noreply.github.com>
2022-06-12 10:31:21 -0500

    Initialize release-google-cloud-trace-v0.36.2

---
+++ google-cloud-container_analysis/CHANGELOG.md
@@ -0,0 +1,63 @@
+# Release History
+
+### 0.4.2 / 2019-11-19
+
+#### Documentation
+
+* Update IAM Policy documentation
+
+### 0.4.1 / 2019-11-06
+
+#### Bug Fixes
+
+* Update minimum runtime dependencies
+
+### 0.4.0 / 2019-10-29
+
+This release require Ruby 2.4 or later.
+
+#### Features
+
+* Use new grafeas gem instead of grafeas-client.
+
+#### Documentation
+
+* Clarify which Google Cloud Platform products support auto-discovered credentials
+
+### 0.3.2 / 2019-10-07
+
+#### Features
+
+* Update grafeas-client dependency to 0.3.
+
+#### Documentation
+
+* Fix role string in IAM Policy JSON example
+* Update IAM Policy class description and sample code
+
+### 0.3.1 / 2019-09-04
+
+#### Documentation
+
+* Update IAM documentation
+  * Update GetPolicyOption#requested_policy_version docs
+  * Un-deprecate Policy#version
+
+### 0.3.0 / 2019-08-23
+
+#### Features
+
+* Add occurrence_path helper
+
+#### Documentation
+
+* Update documentation
+
+### 0.2.0 / 2019-07-08
+
+* Support obtaining grafeas client from the container_analysis client
+* Support overriding service host and port
+
+### 0.1.0 / 2019-06-21
+
+* Initial release
---
+++ google-cloud-storage/test/google/cloud/storage/file/signer_v4_test.rb
@@ -0,0 +1,54 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "helper"
+require "json"
+require_relative "../../../../../conformance/v1/proto/google/cloud/conformance/storage/v1/tests_pb.rb"
+
+class SignerV4Test < MockStorage
+  def setup
+    account_file_path = File.expand_path "../../../../../conformance/v1/test_service_account.not-a-test.json", __dir__
+    account = JSON.parse File.read(account_file_path)
+    credentials.issuer = account["client_email"]
+    credentials.signing_key = OpenSSL::PKey::RSA.new account["private_key"]
+  end
+
+  def self.build_test_for test, index
+    define_method("test_#{index}: #{test.description}") do
+      # start: test method body
+      signer = Google::Cloud::Storage::File::SignerV4.new test.bucket,
+                                                          test.object,
+                                                          storage.service
+      Time.stub :now, SignerV4Test.timestamp_to_time(test.timestamp) do
+        # method under test
+        signed_url = signer.signed_url method: test["method"],
+                                       expires: test.expiration,
+                                       headers: test.headers
+
+        signed_url.must_equal test.expectedUrl
+      end
+      # end: test method body
+    end
+  end
+
+  def self.timestamp_to_time timestamp
+    ::Time.at(timestamp.nanos * 10**-9 + timestamp.seconds)
+  end
+end
+
+file_path = File.expand_path "../../../../../conformance/v1/v4_signatures.json", __dir__
+test_file = Google::Cloud::Conformance::Storage::V1::TestFile.decode_json File.read(file_path)
+test_file.signing_v4_tests.each_with_index do |test, index|
+  SignerV4Test.build_test_for test, index
+end
